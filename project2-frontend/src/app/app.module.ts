f17b87a883356cd06049dd8a478e3a29a028d197
Kurtis <kreid8@kent.edu>
2022-09-02 14:38:56 -0400

    bootstrap added

--- project2-frontend/src/app/app.module.ts
+++ project2-frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GradeFormComponent } from './components/grade-form/grade-form.component';
+import { GradesComponent } from './components/grades/grades.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { StudentFormComponent } from './components/student-form/student-form.component';
+import { StudentsComponent } from './components/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginPageComponent,
+    StudentFormComponent,
+    StudentsComponent,
+    GradeFormComponent,
+    GradesComponent
   ],
   imports: [
     BrowserModule,
