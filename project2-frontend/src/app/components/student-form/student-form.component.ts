ab80bb11aa3b62e55dd6524b44b0dd63ce09dda3
Matthew Schulte <mtthwschulte@gmail.com>
2022-09-05 17:02:17 -0500

    most of student form ts, basic student form html

--- project2-frontend/src/app/components/student-form/student-form.component.ts
+++ project2-frontend/src/app/components/student-form/student-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Student } from 'src/app/models/student';
+import { StudentutilService } from 'src/app/services/studentutil.service';
 
 @Component({
   selector: 'app-student-form',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private studentService:StudentutilService, private router:Router) { }
+
+  firstName:string = "";
+  lastName:string = "";
+  guardianUsername:string = "";
+  savedId:number = 0;
 
   ngOnInit(): void {
+  }
+
+  async register(){
+    const student: Student = {id:0, firstName:this.firstName, lastName:this.lastName, guardianUsername:this.guardianUsername};
+    const savedStudent: Student = await this.studentService.createStudent(student);
+    this.savedId = savedStudent.id;
+    // this.router.navigateByUrl();
   }
 
 }
