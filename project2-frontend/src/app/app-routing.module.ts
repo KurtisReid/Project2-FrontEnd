deeb48790a97d1874b5245eada51bdc436602449
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-09-08 13:59:59 -0700

    Fixed app-routing-module

--- project2-frontend/src/app/app-routing.module.ts
+++ project2-frontend/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { GradesPageComponent } from './components/grades-page/grades-page.component';
-import { StudentsComponent } from './components/students/students.component';
-import { GradesComponent } from './components/grades/grades.component';
+import { StudentPageComponent } from './components/student-page/student-page.component';
+
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'grades-page', component: GradesPageComponent },
-  { path: 'students-page', component: StudentsComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'students-page', component: StudentPageComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
