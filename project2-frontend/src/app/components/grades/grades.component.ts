c335cafd16a4284e25ace51fe98efb4261e3bab8
Matthew Schulte <mtthwschulte@gmail.com>
2022-09-08 15:06:55 -0500

    worked on grade-form, specific student for grade

--- project2-frontend/src/app/components/grades/grades.component.ts
+++ project2-frontend/src/app/components/grades/grades.component.ts
@@ -3,6 +3,7 @@ import { GradeutilService } from 'src/app/services/gradeutil.service';
 import { Grade } from 'src/app/models/grade';
 import { Student } from 'src/app/models/student';
 import { StudentutilService } from 'src/app/services/studentutil.service';
+import { StudenttrackerService } from 'src/app/services/studenttracker.service';
 
 @Component({
   selector: 'app-grades',
@@ -11,7 +12,7 @@ import { StudentutilService } from 'src/app/services/studentutil.service';
 })
 export class GradesComponent implements OnInit {
 
-  constructor(private gradeService:GradeutilService, private studentService:StudentutilService) { }
+  constructor(private gradeService:GradeutilService, private studentService:StudentutilService, private studentTracker:StudenttrackerService) { }
   grades:Grade[] = [];
   students:Student = {id : 0,firstName:"",lastName:"",guardianUsername:""}
   id:number = 1;
@@ -21,8 +22,8 @@ export class GradesComponent implements OnInit {
 
   ngOnInit(): void {
     (async () => {
-      this.grades = await this.gradeService.getGradeByStudentId(1);
-      this.students = await this.studentService.getStudentById(1);
+      this.students = await this.studentTracker.getStudent();
+      this.grades = await this.gradeService.getGradeByStudentId(this.students.id);
       //this.students = {id : 1,firstName:"John",lastName:"Jacob",guardianUsername:"Astor"}
       //this.grades = [{gId: 1, studentId:1, timeReported:1220, note: "he is a monster", behavior:"EVIL"}, {gId: 2 , studentId:1, timeReported:1220, note: "he is petty baby", behavior:"EVIL"}];
     })();
