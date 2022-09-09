25c70be84d840e1bb09b35abbd8a9b5ae13faf1b
dillonconner <dillonconner@gmail.com>
2022-09-09 15:50:20 -0400

    added base url variable

--- project2-frontend/src/app/services/gradeutil.service.ts
+++ project2-frontend/src/app/services/gradeutil.service.ts
@@ -10,34 +10,36 @@ export class GradeutilService {
 
   constructor(private http: HttpClient) { }
 
+  baseUrl:string = "http://localhost:8080";
+
   async getAllGrades():Promise<Grade[]>{
-    const observable = this.http.get<Grade[]>("http://localhost:8080/grades");
+    const observable = this.http.get<Grade[]>("/grades");
     const grades = await firstValueFrom(observable);
     return grades;
   }
 
   async createGrade(grade:Grade):Promise<Grade>{
-    const observable = this.http.post<Grade>("http://localhost:8080/grades", grade);
+    const observable = this.http.post<Grade>(this.baseUrl + "/grades", grade);
     const savedGrade = await firstValueFrom(observable);
     return savedGrade;
   }
 
   async getGradeByStudentId(id:number):Promise<Grade[]>{
     console.log(id);
-    const observable = this.http.get<Grade[]>("http://localhost:8080/grades/" + id);
+    const observable = this.http.get<Grade[]>(this.baseUrl + "/grades/" + id);
     const grades = await firstValueFrom(observable);
     console.log("grade4s" + grades);
     return grades;
   }
 
   async updateGrade(grade:Grade):Promise<Grade>{
-    const observable = this.http.put<Grade>("http://localhost:8080/grades", grade);
+    const observable = this.http.put<Grade>(this.baseUrl + "/grades", grade);
     const savedGrade = await firstValueFrom(observable);
     return savedGrade;
   }
 
   async deleteGrade(id:number):Promise<string>{
-    const observable = this.http.delete<string>("http://localhost:8080/grades/" + id);
+    const observable = this.http.delete<string>(this.baseUrl + "/grades/" + id);
     const grades = await firstValueFrom(observable);
     return grades;
   }
